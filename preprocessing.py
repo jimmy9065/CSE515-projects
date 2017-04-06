16c21ebb0623720f07a6797c908038a2ed17383b
LYZ <jimmy9065@hotmail.com>
2017-04-06 02:38:02 -0500

    changed pics size

--- preprocessing.py
+++ preprocessing.py
@@ -12,28 +12,32 @@ files = os.listdir(path_train)
 
 # only extract cat img
 key = 'dog'
-cat_files = [f for f in files if re.search('^'+key, f)]
+img_files = [f for f in files if re.search('^'+key, f)]
 # images = [misc.imread(path_train+f) for f in files if re.search('^'+key, f)]
-print(len(cat_files))
+print(len(img_files))
+
+len_x = 376
+len_y = 500
 
 images = []
-for i in range(len(cat_files)):
-    image = misc.imread(path_train+cat_files[i])
-    shape_x = image.shape[0]
-    shape_y = image.shape[1]
-    ratio = image.shape[0]/image.shape[1]
-    if ratio <= 1.55 and ratio >= 0.6:
-        x = min(375, shape_x)
-        y = min(500, shape_y)
-        image = misc.imresize(image, (x, y))
-        image = np.lib.pad(
-                image,
-                ((0, 375-x), (0, 500-y), (0, 0)),
-                'constant',
-                constant_values=0)
-        # print(image.shape)
-        images.append(image)
-        misc.imsave(path_data+'pics/'+key+str(i+1)+'.jpeg', image)
+for key in ['dog', 'cat']:
+    for i in range(len(img_files)):
+        image = misc.imread(path_train+img_files[i])
+        shape_x = image.shape[0]
+        shape_y = image.shape[1]
+        ratio = image.shape[0]/image.shape[1]
+        if ratio <= 1.55 and ratio >= 0.6:
+            x = min(len_x, shape_x)
+            y = min(len_y, shape_y)
+            image = misc.imresize(image, (x, y))
+            image = np.lib.pad(
+                    image,
+                    ((0, len_x-x), (0, len_y-y), (0, 0)),
+                    'constant',
+                    constant_values=0)
+            # print(image.shape)
+            images.append(image)
+            misc.imsave(path_data+'pics/'+key+str(i+1)+'.jpeg', image)
 
 images = np.array(images)
 print('info of images')
@@ -45,10 +49,12 @@ print('max size:')
 print(max(shapes[:, 0]), min(shapes[:, 0]))
 print(max(shapes[:, 1]), min(shapes[:, 1]))
 
+sys.exit()
+
 print('saving')
+
 np.save(path_data+key, images)
 
-sys.exit()
 # split the data into 4 part, otherwise it might be hard to load into RAM
 images_parts = np.split(images, 2)
 
